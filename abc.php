6694d9014f49dbe44d20c340699020a2391e2b67
O.Gabriel <gabitzu_002@yahoo.com>
2017-03-12 22:18:32 +0200

    portofolio
    p

---
+++ abc.php
@@ -0,0 +1,27 @@
+<?php
+  $host="sql207.byethost7.com"
+  $dbuser="b7_19688008"
+  $pass="MLAwoeho4d."
+  $dbname="b7_19688008_user_db"
+$conn=msqli_connect($host,$dbuser,$pass,$dbname);
+if(mysqli_connect_errno())
+{
+  die("Connection Failed" . mysqli_connect_error());
+}
+else
+{
+  echo "Connected to database {$dbname}"
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	dfgdsgjkdhsjkghsdgdfsg
+</body>
+</html>
